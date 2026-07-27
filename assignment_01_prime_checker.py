9a558db6a7a0627d839c9c554a4174e286abb909
Wonder Owusu Ansah <woansah004@st.ug.edu.gh>
2026-07-27 17:20:03 +0000

    Update assignment_01_prime_checker.py

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -33,7 +33,7 @@
 
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
-# =============================================================================
+
  
 def is_prime(number):
     if number < 2:
@@ -46,9 +46,9 @@ def is_prime(number):
     return True
 
  
-number = int(input("Enter a number = "))
+number = int(input("Enter a number : "))
 if is_prime(number):
     print(f"{number} is a prime number.")
 else:
-    print(f"{number} is not a prime number.")
+    print(f"{number} is NOT a prime number.")
 
